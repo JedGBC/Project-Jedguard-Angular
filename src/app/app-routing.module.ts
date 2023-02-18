e427a8d90f7a0ec131eb5905bda476859fe7820c
Jedguard Batista <jedguard@gmail.com>
2023-02-18 00:59:31 -0300

    Ordendando el app-routing /routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }, { path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule) }, { path: 'payment', loadChildren: () => import('./payment/payment.module').then(m => m.PaymentModule) }];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, 
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }, 
+  { path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule) }, 
+  { path: 'payment', loadChildren: () => import('./payment/payment.module').then(m => m.PaymentModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
